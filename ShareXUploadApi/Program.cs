44451615b248ec26ef5458c619b579e88da23aed
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2022-07-28 08:33:17 +0200

    activated swagger dashboard

--- ShareXUploadApi/Program.cs
+++ ShareXUploadApi/Program.cs
@@ -15,13 +15,12 @@ using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
 
 builder.Services.AddHttpClient();
@@ -88,11 +87,11 @@ app.MapGet("sharex/getsharelink",
 
 
 // Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
